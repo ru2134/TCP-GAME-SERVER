e48b33ecf08c5502fd616986e570e1a93e55cc45
ru2134 <167045410+ru2134@users.noreply.github.com>
2024-06-24 18:04:51 +0900

    🎨	:art:	코드의 구조/형태 개선

--- src/init/index.js
+++ src/init/index.js
@@ -1,14 +1,19 @@
 // 서버 초기화 작업
-import { loadGameAssets } from "./assets.js";
+import { loadGameAssets } from './assets.js';
+import { loadProtos } from './loadProtos.js';
+import { testAllConnections } from '../utils/db/testConnection.js';
+import pools from '../db/database.js';
 
-const initServer = async ()=>{
-    try {
-        await loadGameAssets();
-        //다음 작업
-    } catch (error) {
-        console.error(error);
-        Process.exit(1); // 오류 발생 시 프로세스를 종료합니다. 
-    }
+const initServer = async () => {
+  try {
+    await loadGameAssets();
+    await loadProtos();
+    await testAllConnections(pools);
+    // 다음 작업
+  } catch (e) {
+    console.error(e);
+    process.exit(1); // 오류 발생 시 프로세스 종료
+  }
 };
 
 export default initServer;
--- src/server.js
+++ src/server.js
@@ -1,28 +1,14 @@
 import net from 'net';
 import initServer from './init/index.js';
+import { config } from './config/config.js';
+import { onConnection } from './events/onConnection.js';
 
-const PORT = 5555;
-
-const server = net.createServer((socket) => {
-  console.log(`Client connected from: ${socket.remoteAddress}:${socket.remotePort}`);
-
-  socket.on('data', (data) => {
-    console.log(data);
-  });
-
-  socket.on('end', () => {
-    console.log('Client disconnected');
-  });
-
-  socket.on('error', (err) => {
-    console.error('Socket error:', err);
-  });
-});
+const server = net.createServer(onConnection);
 
 initServer()
   .then(() => {
-    server.listen(PORT, () => {
-      console.log(`Echo server listening on port ${PORT}`);
+    server.listen(config.server.port, config.server.host, () => {
+      console.log(`서버가 ${config.server.host}:${config.server.port}에서 실행 중입니다.`);
       console.log(server.address());
     });
   })
